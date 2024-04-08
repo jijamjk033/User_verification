0647ed51d51d669d0b3c27d7e8d009e5366181b1
jijamjk033 <jijamjk333@gmail.com>
2024-04-08 17:06:16 +0530

    updation

--- src/route/userRoute.js
+++ src/route/userRoute.js
@@ -16,6 +16,7 @@ user_route.get('/register',auth.isLogout,userController.loadRegister);
 user_route.post('/register',userController.insertUser);
 user_route.get('/profile',auth.isLogin,userController.loadprofile);
 user_route.post('/profile', multer.uploadUser.single("profile_image"),userController.userEdit);
+user_route.post('/uploadProfileImage', multer.uploadUser.single("profile_image"),userController.updateUserProfilepic);
 user_route.get('/togglePrivacy', auth.isLogin, userController.togglePrivacy);
 //Logout
 user_route.post('/logout',auth.isLogin,userController.userLogout);
